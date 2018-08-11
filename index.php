379370a16a53eb9ba4d3efc53d1a2c15cea11a7b
Weston Ruter <weston@xwp.co>
2018-08-11 15:53:39 -0700

    Add POT file and load for plugin translation strings

--- index.php
+++ index.php
@@ -8,9 +8,18 @@
  * Author URI:   https://mkaz.blog/
  * License:      GPL2
  * License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:  code-syntax-block
  *
  * @package Code_Syntax_Block
  */
+
+/**
+ * Load text domain.
+ */
+function mkaz_load_plugin_textdomain() {
+	load_plugin_textdomain( 'code-syntax-block', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+}
+add_action( 'plugins_loaded', 'mkaz_load_plugin_textdomain' );
 
 /**
  * Enqueue assets for editor portion of Gutenberg
@@ -24,7 +33,7 @@ function mkaz_code_syntax_editor_assets() {
 	wp_enqueue_script(
 		'mkaz-code-syntax',
 		plugins_url( $block_path, __FILE__ ),
-		array( 'wp-blocks', 'wp-element' ),
+		array( 'wp-blocks', 'wp-element', 'wp-i18n' ),
 		filemtime( plugin_dir_path( __FILE__ ) . $block_path )
 	);
 
@@ -34,6 +43,13 @@ function mkaz_code_syntax_editor_assets() {
 		plugins_url( $editor_style_path, __FILE__ ),
 		array( 'wp-blocks' ),
 		filemtime( plugin_dir_path( __FILE__ ) . $editor_style_path )
+	);
+
+	// Prepare Jed locale data.
+	$locale_data = gutenberg_get_jed_locale_data( 'code-syntax-block' );
+	wp_add_inline_script(
+		'wp-i18n',
+		sprintf( 'wp.i18n.setLocaleData( %s, "code-syntax-block" );', wp_json_encode( $locale_data ) )
 	);
 
 }
