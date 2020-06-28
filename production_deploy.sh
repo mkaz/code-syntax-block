b54a4c85011c183445bfa2c77984b6e49dea43e2
edward <son@binhsonnguyen.com>
2020-06-28 19:59:53 +0700

    fetch remotes before checkout

--- production_deploy.sh
+++ production_deploy.sh
@@ -3,8 +3,8 @@
 set -e
 
 cd /home/ubuntu/wordpress/syntaxism-code-block-plugin
-git checkout master
 git fetch --all
+git checkout master
 git reset --hard origin/master
 docker run --rm -it -v "$PWD":/app -w /app node:lts npm install
 docker run --rm -it -v "$PWD":/app -w /app node:lts npm run build
