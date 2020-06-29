d33b43e1a996a2b62a3be72e7a08ef5d508f7dac
edward <son@binhsonnguyen.com>
2020-06-29 15:12:40 +0700

    use solarized light theme

--- index.php
+++ index.php
@@ -136,7 +136,7 @@ add_action( 'enqueue_block_assets', function() {
  */
 function mkaz_prism_theme_css( $rtnPath = false ) {
 
-	$default_path = '/assets/prism/themes/prism-duotone-light.css';
+	$default_path = '/assets/prism/themes/prism-solarized-light.css';
 	/**
 	 * Filter the theme directory path used for overriding css path
 	 *
