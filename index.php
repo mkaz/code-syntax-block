332e209713555c351c23b8d04df5214ac83d7ebc
AH-dark <ssnight_0325ahlw@yeah.net>
2021-07-15 14:55:53 +0800

    更改jsDelivr调用地址

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 const MKAZ_CODE_SYNTAX_BLOCK_VERSION = '2.0.4';
 const MKAZ_CODE_SYNTAX_DEFAULT_SCHEME = 'prism-a11y-dark';
 const MKAZ_CODE_SYNTAX_COLOR_SCHEMES = ['prism-a11y-dark', 'prism-ghcolors', 'prism-nord', 'prism-onedark'];
-const Code_Syntax_Block_jsdelivr_URL = "https://cdn.jsdelivr.net/gh/AH-dark/code-syntax-block@trunk/";
+const Code_Syntax_Block_jsdelivr_URL = "https://cdn.jsdelivr.net/gh/AH-dark/code-syntax-block@2.0.4/";
 require dirname(__FILE__) . '/prism-languages.php';
 require dirname(__FILE__) . '/rest-api.php';
 
