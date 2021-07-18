6d8d39da54a0bafecb64c9528ce67a1933ab3240
AH-dark <ssnight_0325ahlw@yeah.net>
2021-07-18 18:06:36 +0800

    Formatting the code

--- assets/prism/prism-settings.js
+++ assets/prism/prism-settings.js
@@ -1,3 +1,2 @@
-
 // Prism settings to configure auto-loader
 Prism.plugins.autoloader.languages_path = prism_settings.pluginUrl + 'assets/prism/prism-components/';
--- assets/prism/prism-title.js
+++ assets/prism/prism-title.js
@@ -1,30 +1,29 @@
-
 // Prism plugin for title/filename
-( function() {
-	const hook = function( env ) {
-		const pre = env.element.parentNode;
-		if ( ! pre || ! /pre/i.test( pre.nodeName ) ) {
-			return;
-		}
+(function() {
+    const hook = function(env) {
+        const pre = env.element.parentNode;
+        if (!pre || !/pre/i.test(pre.nodeName)) {
+            return;
+        }
 
-		// check for title div already exists
-		// autoload runs twice for languages
-		const matches = pre.getElementsByClassName( 'prism-titlename' );
-		if ( matches.length > 0 ) {
-			return;
-		}
+        // check for title div already exists
+        // autoload runs twice for languages
+        const matches = pre.getElementsByClassName('prism-titlename');
+        if (matches.length > 0) {
+            return;
+        }
 
-		// Create wrapper for <pre> to prevent scrolling toolbar with content
-		if ( pre.getAttribute( 'title' ) ) {
-			const title = document.createElement( 'div' );
-			title.classList.add( 'prism-titlename' );
-			title.innerHTML = pre.getAttribute( 'title' );
-			pre.prepend( title );
-		}
-	};
+        // Create wrapper for <pre> to prevent scrolling toolbar with content
+        if (pre.getAttribute('title')) {
+            const title = document.createElement('div');
+            title.classList.add('prism-titlename');
+            title.innerHTML = pre.getAttribute('title');
+            pre.prepend(title);
+        }
+    };
 
-	/**
-	 * Register the toolbar with Prism.
-	 */
-	Prism.hooks.add( 'complete', hook );
-}() );
+    /**
+     * Register the toolbar with Prism.
+     */
+    Prism.hooks.add('complete', hook);
+}());
