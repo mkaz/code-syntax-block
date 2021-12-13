78eab9aa90a46eb670127dd9e1880055e0016642
Marcus Kazmierczak <marcus@mkaz.com>
2021-12-12 17:43:23 -0800

    Remove pre-WP 5.6 from save fn

--- src/save.js
+++ src/save.js
@@ -8,30 +8,18 @@ const save = ( { attributes } ) => {
 	cls = attributes.language ? 'language-' + attributes.language : '';
 	cls = attributes.lineNumbers ? cls + ' line-numbers' : cls;
 
-	// WP 5.6 / GB 9.2
-	if ( useBlockProps ) {
-		const blockProps = useBlockProps.save( { title: attributes.title } );
-		return (
-			<>
-				<pre { ...blockProps }>
-					<RichText.Content
-						tagName="code"
-						value={ attributes.content }
-						lang={ attributes.language }
-						className={ cls }
-					/>
-				</pre>
-			</>
-		);
-	}
-
-	// Backward compatibility < WP 5.6
+	const blockProps = useBlockProps.save( { title: attributes.title } );
 	return (
-		<pre className="wp-block-code" title={ attributes.title }>
-			<code lang={ attributes.language } className={ cls }>
-				{ attributes.content }
-			</code>
-		</pre>
+		<>
+			<pre { ...blockProps }>
+				<RichText.Content
+					tagName="code"
+					value={ attributes.content }
+					lang={ attributes.language }
+					className={ cls }
+				/>
+			</pre>
+		</>
 	);
 };
 
